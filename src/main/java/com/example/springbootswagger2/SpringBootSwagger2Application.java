cca698c067d56d4518877c3c9f0219f3a9d332d5
karthik78180 <karthik78180@gmail.com>
2019-07-21 12:16:47 -0500

    Spring Boot documentation using Swagger UI.

---
+++ src/main/java/com/example/springbootswagger2/SpringBootSwagger2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootswagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootSwagger2Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwagger2Application.class, args);
+	}
+}
